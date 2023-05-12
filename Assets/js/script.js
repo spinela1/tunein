7f7bef1545a52f71200dbd66d791184e29ecaced
spinela1 <123512024+spinela1@users.noreply.github.com>
2023-05-12 15:42:13 -0400

    New assets upload

---
+++ Assets/js/script.js
@@ -0,0 +1,31 @@
+new Splide( '.splide' ).mount();
+
+function dNote() {
+    let dSound = new Audio('Assets/imgs/D-Note.mp3');
+    dSound.play();
+}
+
+function aNote() {
+    let aSound = new Audio('Assets/imgs/A-Note.mp3');
+    aSound.play();
+}
+
+function eNote() {
+    let dSound = new Audio('Assets/imgs/E-Note.mp3');
+    dSound.play();
+}
+
+function gNote() {
+    let dSound = new Audio('Assets/imgs/G-Note.mp3');
+    dSound.play();
+}
+
+function bNote() {
+    let dSound = new Audio('Assets/imgs/B-Note.mp3');
+    dSound.play();
+}
+
+function higheNote() {
+    let dSound = new Audio('Assets/imgs/highE-Note.mp3');
+    dSound.play();
+}
